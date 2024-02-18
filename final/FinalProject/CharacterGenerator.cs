bd18d1ede4da76d31949df8e2ee26bf69a3c2c14
IncendiumVulpus <whaley004@gmail.com>
2024-02-17 18:35:13 -0800

    minro change

--- final/FinalProject/CharacterGenerator.cs
+++ final/FinalProject/CharacterGenerator.cs
@@ -4,13 +4,11 @@ public class CharacterGenerator
 {
     public Character GenerateCharacter()
     {
-        // Implementation to generate a random character
         throw new NotImplementedException();
     }
 
     public Character CustomizeCharacter(string name, Race race, Class characterClass)
     {
-        // Implementation to customize a character
         throw new NotImplementedException();
     }
 }
